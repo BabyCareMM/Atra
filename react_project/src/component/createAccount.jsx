269f90031b05d0a342cf8aa32a62c216ce52561c
maweltscher@gmail.com <maweltscher@gmail.com>
2021-02-14 22:43:54 +0200

    login and ceate account component

---
+++ react_project/src/component/createAccount.jsx
@@ -0,0 +1,30 @@
+export default function CreateAccount(){
+
+    return (<>
+        <div className="wrapper fadeInDown">
+            <div id="formContent">
+        {/* // <!-- Tabs Titles --> */}
+    
+        {/* // <!-- Icon --> */}
+            <div class="fadeIn first">
+            <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
+            </div>
+    
+        {/* // <!-- createAccount Form --> */}
+            <form>
+            <input type="text" id="name" class="fadeIn first" name="name" placeholder="name"/>
+            <input type="text" id="email" class="fadeIn second" name="email" placeholder="email"/>
+            <input type="text" id="password" class="fadeIn third" name="createAccount" placeholder="password"/>
+            <input type="submit" class="fadeIn fourth" value="Create Account"/>
+            </form>
+    
+        {/* // <!-- Remind Passowrd --> */}
+        <div id="formFooter">
+          <a class="underlineHover" href="#">Forgot Password?</a>
+        </div>
+    
+      </div>
+    </div> 
+    </>)
+    
+    }
