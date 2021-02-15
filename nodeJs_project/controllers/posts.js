a7b2f743132bfd8a9891b057dbc83e071c6c47ff
Mindy Leblanc <lemindy43@gmail.com>
2021-02-16 00:19:42 +0200

    react-service pages

--- nodeJs_project/controllers/posts.js
+++ nodeJs_project/controllers/posts.js
@@ -1,30 +1,30 @@
 const Posts = require('../models/posts')
-const User =require('../models/user')
+const User = require('../models/user')
 
 //gets all the posts the user chose
-const getUsersChosenPosts = async(req,res) =>{
-    try{
+const getUsersChosenPosts = async (req, res) => {
+    try {
         const posts = await User.findById(req.params.id).populate('Posts')
         res.status(200).json(posts)
     }
-    catch(error){
+    catch (error) {
         res.status(400).send(error.message)
     }
 }
-
+//?? chosenPosts i think you have to push in the id not the whole post
 //creates a new post for the user that chose it
-const addChosenPost = async(req,res) =>{
+const addChosenPost = async (req, res) => {
     const post = new Post(req.body)
-    try{
+    try {
         await post.save()
         const user = await User.findById(req.body.userId)
         user.chosenPosts.push(post)
         await user.save()
         res.status(200).json('post chosen successfully')
     }
-    catch(error){
+    catch (error) {
         res.status(400).send(error.message)
     }
 }
 
-module.exports = { getUsersChosenPosts, addChosenPost}
+module.exports = { getUsersChosenPosts, addChosenPost }
